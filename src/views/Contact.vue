4c2410bf8ad2ea05e29332f12540aaa36f41707c
Anna Trzeciak <trzeciakanna@hotmail.com>
2021-03-14 17:49:18 +0100

    feat: added MainInfoView (first step when user want describe what needs)

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,18 +3,27 @@
     <header>
       <div>
         <h1>
-          <strong>szukam</strong><br/>
+          <strong>szukam</strong><br />
           kreatora
         </h1>
       </div>
       <div>
-        <img src="/iPad.png" alt="IPad - aplikacja" title="Kreator - IPad"/>
+        <img src="/iPad.png" alt="IPad - aplikacja" title="Kreator - IPad" />
       </div>
     </header>
     <section class="contact-form-section">
-      <thank-you-view v-if="isVisibleThankYou"/>
-      <call-me-view v-else-if="isVisibleCallMeForm" @form-sent="formSent" :services="services"/>
-      <main-contact-view v-else @show-call-me-form="showCallMeForm"/>
+      <main-info-view v-if="isVisibleMainInfo" @next="openNextView" @prev="openPrevView"/>
+      <thank-you-view v-else-if="isVisibleThankYou" />
+      <call-me-view
+        v-else-if="isVisibleCallMeForm"
+        @form-sent="formSent"
+        :services="services"
+      />
+      <main-contact-view
+        v-else
+        @start-describe="showDescribeForm"
+        @show-call-me-form="showCallMeForm"
+      />
     </section>
   </div>
 </template>
@@ -23,10 +32,12 @@
 import MainContactView from "./ContactViews/MainContactView";
 import CallMeView from "./ContactViews/CallMeView";
 import ThankYouView from "./ContactViews/ThankYouView";
+import MainInfoView from "./ContactViews/MainInfoView";
 
 export default {
   name: "Contact",
   components: {
+    MainInfoView,
     ThankYouView,
     CallMeView,
     MainContactView
@@ -35,10 +46,26 @@ export default {
     return {
       isVisibleThankYou: false,
       isVisibleCallMeForm: false,
-      services: []
+      isVisibleMainInfo: false,
+      services: [],
+      data: {}
     };
   },
   methods: {
+    openNextView (data) {
+      if (this.isVisibleMainInfo) {
+        this.data["contact-details"] = data;
+      }
+    },
+    openPrevView () {
+      if (this.isVisibleMainInfo) {
+        this.isVisibleMainInfo = false;
+      }
+    },
+    showDescribeForm (services) {
+      this.services = services;
+      this.isVisibleMainInfo = true;
+    },
     showCallMeForm (services) {
       this.services = services;
       this.isVisibleCallMeForm = true;
@@ -52,81 +79,81 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .contact-page {
-    header {
-      max-width: 100%;
-      width: calc(100vw - (100vw - 968px) / 2);
-      margin-left: auto;
-      margin-right: 0;
-      display: flex;
-      position: relative;
-      @media (max-width: 678px) {
-        flex-direction: column;
-      }
-
-      div {
-        float: left;
-        height: 462px;
-        box-sizing: border-box;
-        width: 50%;
-
-        @media (max-width: 968px) {
-          height: auto;
-        }
-        @media (max-width: 678px) {
-          width: 100%;
-        }
-
-        &:first-of-type {
-          padding-left: 58px;
-          padding-top: 50px;
-          background-image: url("/top_bg2.jpeg");
-          background-size: cover;
-          background-position: center right;
-          display: flex;
-          align-items: flex-end;
-          @media (max-width: 678px) {
-            padding-left: 25px;
-          }
-        }
-
-        &:nth-of-type(2) {
-          padding-top: 50px;
-          background-image: url("/top_bg1.jpeg");
-          background-size: cover;
-          background-position: center left;
-          @media (max-width: 1100px) {
-            display: flex;
-            justify-content: center;
-          }
-
-          img {
-            position: absolute;
-            left: calc(50% - 210px);
-            bottom: 50px;
-            @media (max-width: 1100px) {
-              position: static;
-              margin-bottom: 50px;
-              max-width: 90%;
-            }
-          }
-        }
-
-        h1 {
-          font-size: 65px;
-          font-weight: normal;
-          color: #fff;
-          @media (max-width: 678px) {
-            font-size: 50px;
-          }
-        }
-      }
+.contact-page {
+  header {
+    max-width: 100%;
+    width: calc(100vw - (100vw - 968px) / 2);
+    margin-left: auto;
+    margin-right: 0;
+    display: flex;
+    position: relative;
+    @media (max-width: 678px) {
+      flex-direction: column;
     }
 
-    .contact-form-section {
-      margin: 130px auto;
-      max-width: 968px;
-      width: 100%;
+    div {
+      float: left;
+      height: 462px;
+      box-sizing: border-box;
+      width: 50%;
+
+      @media (max-width: 968px) {
+        height: auto;
+      }
+      @media (max-width: 678px) {
+        width: 100%;
+      }
+
+      &:first-of-type {
+        padding-left: 58px;
+        padding-top: 50px;
+        background-image: url("/top_bg2.jpeg");
+        background-size: cover;
+        background-position: center right;
+        display: flex;
+        align-items: flex-end;
+        @media (max-width: 678px) {
+          padding-left: 25px;
+        }
+      }
+
+      &:nth-of-type(2) {
+        padding-top: 50px;
+        background-image: url("/top_bg1.jpeg");
+        background-size: cover;
+        background-position: center left;
+        @media (max-width: 1100px) {
+          display: flex;
+          justify-content: center;
+        }
+
+        img {
+          position: absolute;
+          left: calc(50% - 210px);
+          bottom: 50px;
+          @media (max-width: 1100px) {
+            position: static;
+            margin-bottom: 50px;
+            max-width: 90%;
+          }
+        }
+      }
+
+      h1 {
+        font-size: 65px;
+        font-weight: normal;
+        color: #fff;
+        @media (max-width: 678px) {
+          font-size: 50px;
+        }
+      }
     }
   }
+
+  .contact-form-section {
+    margin: 130px auto;
+    max-width: 968px;
+    width: 100%;
+  }
+}
 </style>
---
+++ src/views/ContactViews/MainInfoView.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="contact-form--main-info-view">
+    <form class="form" @submit.prevent="submit">
+      <contact-form>
+        <template v-slot:header>
+          <h2>
+            <strong>Wypełnij ten formularz,</strong> dzięki czemu <br />
+            <strong>przygotujemy rozwiązania</strong> na miarę Twoich potrzeb
+          </h2>
+        </template>
+        <template v-slot:content>
+          <label>
+            Imię (wymagane)
+            <span
+              class="error"
+              v-if="$v.form.name.$error && !$v.form.name.required"
+            >To pole jest wymagane</span
+            >
+            <span
+              class="error"
+              v-else-if="$v.form.name.$error && !$v.form.name.minLength"
+            >Minimalna długość to 4 znaki</span
+            >
+            <input v-model="form.name" type="text"  />
+          </label>
+          <label>
+            Nazwisko (niewymagane)
+            <input v-model="form.surname" type="text" /> </label
+          ><label>
+            Email (wymagane)
+          <span
+            class="error"
+            v-if="$v.form.email.$error && !$v.form.email.required"
+          >To pole jest wymagane</span
+          >
+          <span
+            class="error"
+            v-else-if="$v.form.email.$error && !$v.form.email.email"
+          >Wprowadzono niepoprawny format adresu email</span
+          >
+            <input v-model="form.email" type="email"  />
+          </label>
+          <label class="contact-form__agree">
+            <input v-model="form.agree" type="checkbox"  />
+            <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+              <span
+                class="error"
+                v-if="$v.form.agree.$error && !$v.form.agree.checked"
+              ><br />Zgoda jest wymagana
+              </span>
+            </span>
+          </label>
+        </template>
+        <template v-slot:footer>
+          <button type="button" class="outline" @click="$emit('prev')">WSTECZ</button>
+          <button type="submit">DALEJ</button>
+        </template>
+      </contact-form>
+    </form>
+  </div>
+</template>
+
+<script>
+import Vuelidate from "vuelidate";
+import { minLength, required, email } from "vuelidate/lib/validators";
+
+import ContactForm from "../../components/ContactForm";
+import Vue from "vue";
+Vue.use(Vuelidate);
+const checked = value => value === true;
+
+export default {
+  name: "MainInfoView",
+  components: { ContactForm },
+  validations: {
+    form: {
+      name: {
+        required,
+        minLength: minLength(4)
+      },
+      email: {
+        required,
+        email
+      },
+      agree: {
+        checked
+      }
+    }
+  },
+  data: function () {
+    return {
+      form: {
+        name: "",
+        surname: "",
+        email: "",
+        agree: false
+      }
+    };
+  },
+  methods: {
+    async submit () {
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
+        this.$emit("next", this.form);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.contact-form {
+}
+</style>
